e646fe63cc829ac2752fc58d1ec67b74ed9af76c
nmessa <nmessa@sau16.org>
2022-04-03 09:24:56 -0400

    Add files via upload

---
+++ Lab Exercise 4.8.2022/Test/Test/Form1.cs
@@ -0,0 +1,33 @@
+﻿//Array of labels demo
+//The array is created in the constructor
+//It is made global in order to be accessible to all functions
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        Label[] arr; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Create an array of 5 labels
+            arr = new Label[] { label1, label2, label3, label4, label5 };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                arr[i].Text = i.ToString();
+        }
+    }
+}
