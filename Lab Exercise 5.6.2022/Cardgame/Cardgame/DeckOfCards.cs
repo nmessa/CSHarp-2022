da34343aafeef33d6a5dc79c01720b9f11feaf7d
nmessa <nmessa@sau16.org>
2022-05-01 08:17:00 -0400

    Add files via upload

---
+++ Lab Exercise 5.6.2022/Cardgame/Cardgame/DeckOfCards.cs
@@ -0,0 +1,35 @@
+﻿//Lab Exercise 5.6.2022
+//DeckOfCards Class
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cardgame
+{
+    class DeckOfCards
+    {
+        //Add code here
+
+
+        public DeckOfCards()
+        {
+            //Add code here
+
+        }
+
+        public void shuffle()
+        {
+            //Add code here
+
+        }
+
+        public PlayingCard deal()
+        {
+            //Add code here
+
+        }
+    }
+}
